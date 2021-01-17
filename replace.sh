b627d280742b55b0a51cc1ec78c2bd4badb561b9
wgs <wgshuaiit@163.com>
2021-01-17 23:44:05 +0800

    测试copy

--- replace.sh
+++ replace.sh
@@ -2,11 +2,14 @@
 #!/bin/bash
 # 先判断文件是否存在，如果存在，则备份
 # cd  /root/.jenkins/workspace/gitHub/target 可以用下面代替
-# cd $PROJ_PATH/target
-cd  /root/.jenkins/workspace/gitHub/target
+ cd $PROJ_PATH/target
+
 # 备份
 cp spring-boot-docker-order.war /opt/soft/spring-boot-docker-order.war
 # 备份重命名
-mv /opt/soft/spring-boot-docker-order.war spring-boot-docker-order.war.`date +%Y%m%d%H%M%S`
+cd /opt/soft
+mv spring-boot-docker-order.war spring-boot-docker-order.war.`date +%Y%m%d%H%M%S`
  echo "--------------copy------------"
+cd $PROJ_PATH/target
+echo "--------------copy end------------"
 #此处 /opt/soft/spring-boot-docker-order.war根据自己实际jar包名称和路径修改
