72b869631d263e759d7a1a1d3aa8d1c6d0bb6584
wgs <wgshuaiit@163.com>
2021-01-17 23:14:14 +0800

    修改脚本

--- replace.sh
+++ replace.sh
@@ -1,10 +1,11 @@
 #replace.sh 用于将上次构建的结果备份，然后将新的构建结果移动到合适的位置
 #!/bin/bash
 # 先判断文件是否存在，如果存在，则备份
+# cd  /root/.jenkins/workspace/gitHub/target
+cd $PROJ_PATH/gitHub/target
 
-cd  /root/.jenkins/workspace/gitHub/target
 # 备份
-cp /root/.jenkins/workspace/gitHub/target/spring-boot-docker-order.war /opt/soft/spring-boot-docker-order.war
+cp spring-boot-docker-order.war /opt/soft/spring-boot-docker-order.war
 # 备份重命名
 mv /opt/soft/spring-boot-docker-order.war spring-boot-docker-order.war.`date +%Y%m%d%H%M%S`
 
