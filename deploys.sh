415548021279059e27f29284c79b00b8c628742d
wgs <wgshuaiit@163.com>
2021-01-18 15:16:07 +0800

    测试触发器

---
+++ deploys.sh
@@ -0,0 +1,9 @@
+BUILD_ID=DONTKILLME
+. /etc/profile
+export PROJ_PATH=`pwd`
+
+sh $PROJ_PATH/stop.sh
+sh $PROJ_PATH/replace.sh
+
+cd $PROJ_PATH/target
+nohup java -jar spring-boot-docker-order.war &
